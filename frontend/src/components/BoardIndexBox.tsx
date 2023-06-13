18b38abaabff11e3e696a02bccac2594f7937cc4
Elias <79339968+hunaja@users.noreply.github.com>
2023-06-13 23:29:29 +0300

    Handle empty threads

--- frontend/src/components/BoardIndexBox.tsx
+++ frontend/src/components/BoardIndexBox.tsx
@@ -19,56 +19,54 @@ export default function BoardIndexBox({
     },
 }: Props) {
     const board = useBoard();
-    const timeAgo = useTimeAgo(opPost?.createdAt ?? "");
+    const timeAgo = useTimeAgo(opPost?.createdAt ?? thread.bumpedAt);
 
     if (!board) return null;
 
     return (
-        opPost && (
-            <div
-                className="bg-white p-2 h-72 border-2 border-purple-200 flex flex-col justify-between h-100 overflow-hidden break-words"
-                key={thread.id}
-                tabIndex={thread.bumpedAt}
+        <div
+            className="bg-white p-2 h-72 border-2 border-purple-200 flex flex-col justify-between h-100 overflow-hidden break-words"
+            key={thread.id}
+            tabIndex={thread.bumpedAt}
+        >
+            <Link
+                className="text-black shrink overflow-hidden grow"
+                to={`/${board.path}/${thread.number}`}
             >
-                <Link
-                    className="text-black shrink overflow-hidden grow"
-                    to={`/${board.path}/${thread.number}`}
-                >
-                    {opPost.file && (
-                        <img
-                            src={`/files/lauta-thumbnails/${opPost.id}.png`}
-                            alt=""
-                            className="mx-auto rounded"
-                        />
-                    )}
-                    <div className="p-1 h-full">
-                        <h4 className="text-lg">{thread.title}</h4>
-                        {opPost.number === thread.number ? (
-                            opPost.text && formatPost(opPost.text.slice(0, 100))
-                        ) : (
-                            <span className="text-gray-500 bold">
-                                Langan aloitusviesti on poistettu.
-                            </span>
-                        )}
-                    </div>
-                </Link>
-
-                <div className="flex p-1 justify-between text-gray-400 text-xs bg-white w-full shrink-0 grow-0">
-                    <span className="shrink">{timeAgo}</span>
-                    <span className="ml-2">
-                        <span>
-                            <ReplyIcon className="inline-block h-3 w-3 mr-1" />
-                            {thread.replyCount}
+                {opPost?.file && (
+                    <img
+                        src={`/files/lauta-thumbnails/${opPost.id}.png`}
+                        alt=""
+                        className="mx-auto rounded"
+                    />
+                )}
+                <div className="p-1 h-full">
+                    <h4 className="text-lg">{thread.title}</h4>
+                    {opPost?.number === thread.number ? (
+                        opPost.text && formatPost(opPost.text.slice(0, 100))
+                    ) : (
+                        <span className="text-gray-500 bold">
+                            Langan aloitusviesti on poistettu.
                         </span>
-                        {thread.fileReplyCount && (
-                            <span>
-                                <PhotographIcon className="inline-block h-3 w-3 ml-1 mr-1" />
-                                {thread.fileReplyCount}
-                            </span>
-                        )}
-                    </span>
+                    )}
                 </div>
+            </Link>
+
+            <div className="flex p-1 justify-between text-gray-400 text-xs bg-white w-full shrink-0 grow-0">
+                <span className="shrink">{timeAgo}</span>
+                <span className="ml-2">
+                    <span>
+                        <ReplyIcon className="inline-block h-3 w-3 mr-1" />
+                        {thread.replyCount}
+                    </span>
+                    {thread.fileReplyCount && (
+                        <span>
+                            <PhotographIcon className="inline-block h-3 w-3 ml-1 mr-1" />
+                            {thread.fileReplyCount}
+                        </span>
+                    )}
+                </span>
             </div>
-        )
+        </div>
     );
 }
